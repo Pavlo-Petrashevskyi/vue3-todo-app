66248cf71c5e8df6db153be1acdb719fd8d59108
Pavlo Petrashevskyi <dragonthunderaddsdp@gmail.com>
2024-04-30 20:25:18 +0300

    trying to see bulma import

--- src/api/todos.js
+++ src/api/todos.js
@@ -2,14 +2,14 @@ import { client } from '../utils/httpClient'
 
 export const getTodos = () => {
   return client.get('/todos?userId=6342')
-    .then(() => Promise.reject())
+    // .then(() => Promise.reject())
 };
 
 export const createTodo = (title) => {
   return client.post('/todos', {
     title,
     completed: false,
-    userId: 6342,
+    userId: 10905,
   });
 };
 
